b7ae15e1522e6a13110523eb4681b719abb67b44
Amalie S <amaliesmidth@gmail.com>
2018-05-17 14:25:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout(string) {
   console.log (string.toUpperCase())
 }
 
+
+
+
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
