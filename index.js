c9e385253c49acaf5ee899a0c95ba6d83fbe93b5
Amalie S <amaliesmidth@gmail.com>
2018-05-17 14:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ function logWhisper(string) {
   console.log (string.toLowerCase())
 }
 
+function sayHiToGrandma(string) {
+  if uppercase.toUpperCase() === uppercase 
+    return "I can't hear you!"
+  
+  else 
+}
+
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
